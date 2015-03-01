815d6de1cb7e68c079f4fa02fd4844e7d5e3d2ee
Graham <graham@asthecrowflies.co.uk>
2015-03-01 12:05:27 +0000

    Add alembic

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
@@ -10,6 +18,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
